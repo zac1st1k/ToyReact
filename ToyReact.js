3c8556b31139faec897e87278334019691a6e53a
Zac Xu <zacfirst@gmail.com>
2020-07-20 23:22:31 +1000

    feat: add createElement method

--- ToyReact.js
+++ ToyReact.js
@@ -1,6 +1,15 @@
-export let ToyReact = {
-  createElement: () => {
-    console.log(arguments);
-    debugger;
-  }
+export const ToyReact = {
+  createElement(type, attributes, ...children) {
+    let element = document.createElement(type);
+    for (let name in attributes) {
+      element.setAttribute(name, attributes[name]);
+    }
+    for (let child of children) {
+      if (typeof child === "string") {
+        child = document.createTextNode(child);
+      }
+      element.appendChild(child);
+    }
+    return element;
+  },
 }
--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 import { ToyReact } from "./ToyReact";
 
-const a = <div name="div name" />
-const b = <ToyReact name="ToyReact name" />
+const a = <div name="div name" id="3" >
+  <span>Hello</span>
+  <span></span>
+  <span></span>
+</div >
+// const b = <ToyReact name="ToyReact name" />
+// console.log(a);
+document.body.appendChild(a);
