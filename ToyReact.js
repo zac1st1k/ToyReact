12cd78a9c884c85b5bf3080a94bc898f7caf1e72
Zac Xu <zacfirst@gmail.com>
2020-07-21 22:51:12 +1000

    feat: add setState support

--- ToyReact.js
+++ ToyReact.js
@@ -45,6 +45,25 @@ export class Component {
   appendChild(vchild) {
     this.children.push(vchild);
   }
+  setState(state) {
+    let merge = (oldState, newState) => {
+      for (let p in newState) {
+        if (typeof newState[p] === "object") {
+          if (typeof newState[p] !== "object") {
+            oldState[p] = {};
+          }
+          merge(oldState[p], newState[p]);
+        } else {
+          oldState[p] = newState[p];
+        }
+      }
+    };
+    if (!this.state && state) {
+      this.state = {};
+    }
+    merge(this.state, state);
+    console.log(this.state);
+  }
 }
 
 export const ToyReact = {
