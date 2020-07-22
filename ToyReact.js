6a0c2c045e40d8955aa52711fc4fa9ef955f0ef8
Zac Xu <zacfirst@gmail.com>
2020-07-22 22:41:19 +1000

    refactor: prepare for vdom

--- ToyReact.js
+++ ToyReact.js
@@ -1,31 +1,42 @@
 class ElementWrapper {
   constructor(type) {
-    this.root = document.createElement(type);
+    this.type = type;
+    this.props = Object.create(null);
+    this.children = [];
   }
   setAttribute(name, value) {
-    if (name.match(/^on([\s\S]+)$/)) {
-      let eventName = RegExp.$1.replace(/^[\s\S]/, s => s.toLowerCase());
-      this.root.addEventListener(eventName, value);
-    }
-    if (name === "className") {
-      this.root.setAttribute("class", value);
-    }
-    this.root.setAttribute(name, value);
+    this.props[name] = value;
   }
   appendChild(vchild) {
-    let range = document.createRange();
-    if (this.root.children.length) {
-      range.setStartAfter(this.root.lastChild);
-      range.setEndAfter(this.root.lastChild);
-    } else {
-      range.setStart(this.root, 0);
-      range.setEnd(this.root, 0);
-    }
-    vchild.mountTo(range);
+    this.children.push(vchild);
   }
   mountTo(range) {
     range.deleteContents();
-    range.insertNode(this.root);
+    let element = document.createElement(this.type);
+
+    for (let name in this.props) {
+      let value = this.props[name];
+      if (name.match(/^on([\s\S]+)$/)) {
+        let eventName = RegExp.$1.replace(/^[\s\S]/, s => s.toLowerCase());
+        element.addEventListener(eventName, value);
+      }
+      if (name === "className") {
+        element.setAttribute("class", value);
+      }
+      element.setAttribute(name, value)
+    }
+    for (let child of this.children) {
+      let range = document.createRange();
+      if (element.children.length) {
+        range.setStartAfter(element.lastChild);
+        range.setEndAfter(element.lastChild);
+      } else {
+        range.setStart(element, 0);
+        range.setEnd(element, 0);
+      }
+      child.mountTo(range);
+    }
+    range.insertNode(element);
   }
 }
 
